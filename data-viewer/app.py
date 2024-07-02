5e6b70dd0cfbaa162fe44ead89548832dc0f0910
srinivaspach <106605299+srinivaspach@users.noreply.github.com>
2024-07-02 20:36:49 +0100

    Adding auth

--- data-viewer/app.py
+++ data-viewer/app.py
@@ -8,14 +8,14 @@ import modules.formats as formats
 import modules.charts as charts
 import modules.animated as animated
 import modules.utils as utils
-# import auth
+import auth
 
 st.set_page_config(layout="wide")
 st.title("Hierarchical Data Viewer")
 st.caption("Display your hierarchical data with charts and graphs.")
 
-#auth.check_password()
-# auth.check_user_and_password()
+# auth.check_password()
+auth.check_user_and_password()
 
 def getSessionId():
     if "session_id" not in st.session_state:
